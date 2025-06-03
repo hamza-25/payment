60704f9a3f44b063b99759c006870304213b395f
hamza-25 <me677499@gmail.com>
2025-06-03 21:52:10 +0100

    folder structure and setup

---
+++ server.mjs
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
