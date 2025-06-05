56470e329564d3a65a1c407720e77db361f93d64
hamza-25 <me677499@gmail.com>
2025-06-05 10:49:57 +0100

    add getAccessToken paypal method

---
+++ utils/paypalMethods.mjs
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+import axios from 'axios';
+
+dotenv.config();
+
+const { PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET, PAYPAL_API } = process.env;
+
+export const getAccessToken = async () => {
+    const response = await fetch(`${PAYPAL_API}/v1/oauth2/token`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': `Basic ${Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_CLIENT_SECRET}`).toString('base64')}`
+        },
+        body: new URLSearchParams({ 'grant_type': 'client_credentials' })
+    });
+
+    const data = await response.json();
+    return data.access_token;
+}
